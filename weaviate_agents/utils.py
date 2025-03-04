c3a8ba532383a5c22c808bd65186fca13a6181b6
JP Hwang <me@jphwang.com>
2025-03-04 12:20:38 +0000

    Appease mypy

--- weaviate_agents/utils.py
+++ weaviate_agents/utils.py
@@ -1,2 +1,2 @@
 # Importing `print_query_agent_response` function for backwards compatibility
-from weaviate_agents.query.classes.response import print_query_agent_response
+from weaviate_agents.query.classes.response import print_query_agent_response  # noqa: F401
