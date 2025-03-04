6ea1a660b98cd9b1873a590eaabc50b0c6c15bbb
JP Hwang <me@jphwang.com>
2025-03-04 12:24:03 +0000

    Formatting. I should set up pre-commit

--- weaviate_agents/utils.py
+++ weaviate_agents/utils.py
@@ -1,2 +1,4 @@
 # Importing `print_query_agent_response` function for backwards compatibility
-from weaviate_agents.query.classes.response import print_query_agent_response  # noqa: F401
+from weaviate_agents.query.classes.response import (
+    print_query_agent_response,
+)  # noqa: F401
