22704ea0d62e2fad89c5b2287c7a6d5bc57e1374
Dan Jones <danjones@Dans-MacBook-Pro.local>
2025-04-11 10:50:03 +0100

    Refactor query and response into separate method

--- weaviate_agents/personalization/query.py
+++ weaviate_agents/personalization/query.py
@@ -40,6 +40,45 @@ class PersonalizedQuery:
         self.recent_interactions_count = recent_interactions_count
         self.decay_rate = decay_rate
 
+    def near_text(
+        self,
+        query: Union[List[str], str],
+        *,
+        certainty: Union[int, float, None] = None,
+        distance: Union[int, float, None] = None,
+        move_to: Optional[Move] = None,
+        move_away: Optional[Move] = None,
+        limit: Union[int, None] = None,
+        offset: Union[int, None] = None,
+        auto_limit: Union[int, None] = None,
+        filters: Optional[_Filters] = None,
+        # group_by,
+        rerank: Optional[Rerank] = None,
+        target_vector: Optional[TargetVectorJoinType] = None,
+        include_vector: Union[bool, str, List[str]] = False,
+        return_metadata: Optional[METADATA] = None,
+        return_properties: Optional[ReturnProperties[dict]] = None,
+        return_references: Optional[ReturnReferences[dict]] = None,
+    ) -> PersonalizedQueryResponse:
+        query_parameters = NearTextQueryParameters(
+            query=query,
+            certainty=certainty,
+            distance=distance,
+            move_to=move_to,
+            move_away=move_away,
+            limit=limit,
+            offset=offset,
+            auto_limit=auto_limit,
+            filters=filters,
+            rerank=rerank,
+            target_vector=target_vector,
+            include_vector=include_vector,
+            return_metadata=return_metadata,
+            return_properties=return_properties,
+            return_references=return_references,
+        )
+        return self._get_query_response(query_parameters)
+
     def _get_request_data(self, query_parameters: QueryParameters) -> dict[str, Any]:
         query_request = QueryRequest.model_validate(
             {
@@ -58,42 +97,9 @@ class PersonalizedQuery:
             ),
         }
 
-    def near_text(
-        self,
-        query: Union[List[str], str],
-        certainty: Union[int, float, None] = None,
-        distance: Union[int, float, None] = None,
-        move_to: Optional[Move] = None,
-        move_away: Optional[Move] = None,
-        limit: Union[int, None] = None,
-        offset: Union[int, None] = None,
-        auto_limit: Union[int, None] = None,
-        filters: Optional[_Filters] = None,
-        # group_by,
-        rerank: Optional[Rerank] = None,
-        target_vector: Optional[TargetVectorJoinType] = None,
-        include_vector: Union[bool, str, List[str]] = False,
-        return_metadata: Optional[METADATA] = None,
-        return_properties: Optional[ReturnProperties[dict]] = None,
-        return_references: Optional[ReturnReferences[dict]] = None,
-    ):
-        query_parameters = NearTextQueryParameters(
-            query=query,
-            certainty=certainty,
-            distance=distance,
-            move_to=move_to,
-            move_away=move_away,
-            limit=limit,
-            offset=offset,
-            auto_limit=auto_limit,
-            filters=filters,
-            rerank=rerank,
-            target_vector=target_vector,
-            include_vector=include_vector,
-            return_metadata=return_metadata,
-            return_properties=return_properties,
-            return_references=return_references,
-        )
+    def _get_query_response(
+        self, query_parameters: QueryParameters
+    ) -> PersonalizedQueryResponse:
         response = httpx.post(
             self._route,
             headers=self._headers,
