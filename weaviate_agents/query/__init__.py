b13242a027aea50c5712ae7dd22b3b4d16d21a2c
Dan Jones <danjones@Dans-MacBook-Pro.local>
2025-08-15 16:47:55 +0100

    Add searchers to query namespace

--- weaviate_agents/query/__init__.py
+++ weaviate_agents/query/__init__.py
@@ -1,3 +1,4 @@
+from .search import AsyncQueryAgentSearcher, QueryAgentSearcher
 from .query_agent import AsyncQueryAgent, QueryAgent
 
-__all__ = ["AsyncQueryAgent", "QueryAgent"]
+__all__ = ["AsyncQueryAgent", "QueryAgent", "AsyncQueryAgentSearcher", "QueryAgentSearcher"]
