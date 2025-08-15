4dd662fdb6fff204db16279a7dc6ffdf884ba61a
Dan Jones <danjones@Dans-MacBook-Pro.local>
2025-08-15 16:20:44 +0100

    Add additional_filter option to collection config

--- weaviate_agents/query/classes/collection.py
+++ weaviate_agents/query/classes/collection.py
@@ -1,6 +1,9 @@
-from typing import Union
+from typing import Annotated, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from weaviate.collections.classes.filters import _Filters
+
+from weaviate_agents.serialise import serialise_filter
 
 
 class QueryAgentCollectionConfig(BaseModel):
@@ -13,9 +16,14 @@ class QueryAgentCollectionConfig(BaseModel):
             for this specific collection.
         target_vector: Optional target vector name(s) for collections with named vectors.
             Can be a single vector name or a list of vector names.
+        additional_filters: Optional filters to apply when the query is executed, in addition
+            to filters selected by the Query Agent (i.e., there are AND combined).
     """
 
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     name: str
-    tenant: Union[str, None] = None
-    view_properties: Union[list[str], None] = None
-    target_vector: Union[str, list[str], None] = None
+    tenant: Optional[str] = None
+    view_properties: Optional[list[str]] = None
+    target_vector: Optional[str | list[str]] = None
+    additional_filters: Optional[Annotated[_Filters, serialise_filter]] = None
